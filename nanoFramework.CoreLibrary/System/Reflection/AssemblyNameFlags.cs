f5ed62a574a9971669374bd199c4cb31e61efb6b
José Simões <jose.simoes@eclo.solutions>
2025-02-25 05:13:59 +0000

    Remove types to exclude from project file (#234)
    ***NO_CI***
    
    (cherry picked from commit da7ea10fe8512ba76847ca9b4571176bb6522ca2)

--- nanoFramework.CoreLibrary/System/Reflection/AssemblyNameFlags.cs
+++ nanoFramework.CoreLibrary/System/Reflection/AssemblyNameFlags.cs
@@ -13,6 +13,7 @@ namespace System.Reflection
     /// </summary>
     /// <remarks>Available only in mscorlib build with support for System.Reflection.</remarks>
     [Serializable, Flags]
+    [ExcludeType]
     public enum AssemblyNameFlags
     {
         /// <summary>
